32cbfc4fc6d06815a994e21010624ca5718ec29f
mikerol23 <michaelrolincik@gmail.com>
2022-11-09 14:00:31 -0500

    Assign class and style to grid items
     * each div created for grid is given class of "gridItem"
     * "gridItem" class is assigned style to make each 100x100px square

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let grid = document.querySelector(".container");
 function createGrid() {
   for (let i = 0; i < 16; i++) {
     let div = document.createElement("div");
+    div.classList.add("gridItem");
     grid.appendChild(div);
   }
 }
