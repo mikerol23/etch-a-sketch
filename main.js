4ace6fbfb53212fd47fadadeaf3062eebd2fe0aa
mikerol23 <michaelrolincik@gmail.com>
2022-11-09 14:04:34 -0500

    Fix func createGrid to create 256 divs

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 let grid = document.querySelector(".container");
 
 function createGrid() {
-  for (let i = 0; i < 16; i++) {
+  for (let i = 0; i < 256; i++) {
     let div = document.createElement("div");
     div.classList.add("gridItem");
     grid.appendChild(div);
