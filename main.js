4f0a0c5b29324c465a23940dc823e309e60190ef
mikerol23 <michaelrolincik@gmail.com>
2022-11-10 12:14:54 -0500

    Add while loop accounting for invalid user inputs

--- main.js
+++ main.js
@@ -3,7 +3,21 @@ let grid = document.querySelector("#container");
 let btn = document.querySelector(".gridSize");
 btn.addEventListener("click", () => {
   let resolution = window.prompt("Grid density:", 50);
+  while (resolution === NaN || resolution < 1 || resolution > 100) {
+    resolution = window.prompt("Try a number between 1 and 100!", 50);
+  }
   createGrid(resolution);
+
+  // if (resolution == null) {
+  //   return;
+  // }
+  // if (isNaN(resolution) == true) {
+  //   window.prompt("Numbers only please!");
+  // } else if (resolution > 100 || resolution < 1) {
+  //   window.alert("Try a number between 1 and 100!");
+  // } else {
+  //   createGrid(resolution);
+  // }
 });
 
 function createGrid(size) {
